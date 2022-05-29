8e5f7e14d41a98a7dbd859cd843d4990516d8a51
samuel-poinama <samuel.poinama@efrei.net>
2022-05-29 22:13:28 +0200

    main.c modify

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 int main (void) {
     HugeUnsignedInt* hugeNumber = createHugeUnsignedIntFromString ("123456789012345678901234567890");
     printHugeUnsignedInt (hugeNumber);
+	printf("%s", hugeNumber);
     hugeNumber = createHugeUnsignedIntFromString ("0000000123456789012345678901234567890");
     printHugeUnsignedInt (hugeNumber);
+    printf("%s", hugeNumber);
 }
